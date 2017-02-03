f96f313ad100684a45151f998184dc0f0f5aae18
Beaglefoot <ch-e-st@yandex.ru>
2017-02-03 17:07:04 +0300

    feat: Delete functionality on RecipeShow

--- src/components/RecipeShow.jsx
+++ src/components/RecipeShow.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, browserHistory } from 'react-router';
+import { deleteRecipe } from '../actions';
 
 function findRecipe(recipes, id) {
   return recipes.find(recipe => recipe.id === id) || '';
@@ -11,6 +12,8 @@ export function chooseRecipeOnMount(props) {
   const id = parseInt(routeParams.id);
   const chosenRecipe = findRecipe(recipes, id);
 
+  // In case someone attempts access non-existent
+  // recipe via url
   chosenRecipe || browserHistory.push('/');
 
   return chosenRecipe;
@@ -21,6 +24,11 @@ class RecipeShow extends React.Component {
     this.chosenRecipe = chooseRecipeOnMount(this.props);
   }
 
+  handleClick(id) {
+    this.props.deleteRecipe(id);
+    browserHistory.push('/');
+  }
+
   render() {
     const { name, ingredients } = this.chosenRecipe;
 
@@ -29,6 +37,12 @@ class RecipeShow extends React.Component {
         <h3>{name}</h3>
         <p>{ingredients}</p>
         <Link to={`${location.pathname}/edit`} className="btn btn-warning">Edit</Link>
+        <button
+          className="btn btn-danger"
+          onClick={() => this.handleClick(this.chosenRecipe.id)}
+        >
+          Delete
+        </button>
         <Link to="/" className="btn btn-default">Back To Recipe List</Link>
       </div>
     );
@@ -39,4 +53,4 @@ function mapStateToProps({ recipes }) {
   return { recipes: recipes.all };
 }
 
-export default connect(mapStateToProps)(RecipeShow);
+export default connect(mapStateToProps, { deleteRecipe })(RecipeShow);
