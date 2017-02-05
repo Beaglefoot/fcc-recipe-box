161c5ef76b43122bebac08d7183ff1f7fb7a15ef
Beaglefoot <ch-e-st@yandex.ru>
2017-02-05 22:41:56 +0300

    feat: Add signature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Signature from './Signature';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         {this.props.children}
+        <Signature />
       </div>
     );
   }
