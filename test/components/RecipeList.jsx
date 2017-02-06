f252286c4a814f0e55932854ccacc870a3d1863e
Beaglefoot <ch-e-st@yandex.ru>
2017-02-07 00:09:01 +0300

    test: Some tests for RecipeList

---
+++ test/components/RecipeList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { deleteRecipe } from '../../src/actions';
+import reducer from '../../src/reducers';
+import RecipeList from '../../src/components/RecipeList';
+
+describe('<RecipeList />', () => {
+  let store = createStore(reducer);
+  let item;
+
+  beforeEach(() => {
+    item = mount(
+      <Provider store={store}>
+        <RecipeList />
+      </Provider>
+    );
+  });
+
+  it('should render', () => {
+    expect(item).to.exist;
+  });
+
+  it('should have 5 default recipes', () => {
+    expect(item.find('a [type="button"]')).to.have.lengthOf(5);
+  });
+
+  it('should have 4 default recipes after one is deleted from store', () => {
+    store.dispatch(deleteRecipe(1));
+    expect(item.find('a [type="button"]')).to.have.lengthOf(4);
+    store = createStore(reducer);
+  });
+});
--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,5 @@
 import { jsdom } from 'jsdom';
+import moment from 'moment';
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
@@ -6,3 +7,5 @@ global.window = document.defaultView;
 Object.keys(window).forEach(key => {
   if (!(key in global)) global[key] = window[key];
 });
+
+console.log(`---------------${moment().format('HH:mm:ss')}---------------`);
