6207b1d0a43cfbdd441cb66b69a6215bbbee8a30
Beaglefoot <ch-e-st@yandex.ru>
2017-02-08 19:37:47 +0300

    test: Rewrite tests for RecipeShow

--- test/components/RecipeShow_test.jsx
+++ test/components/RecipeShow_test.jsx
@@ -3,24 +3,45 @@ import { expect } from 'chai';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { Router, Route, browserHistory } from 'react-router';
 import reducer from '../../src/reducers';
 import RecipeShow from '../../src/components/RecipeShow';
 
 describe('<RecipeShow />', () => {
   let store = createStore(reducer);
   let item;
-  const routes = <Route path="recipe/:id" component={RecipeShow} />;
 
   beforeEach(() => {
     item = mount(
       <Provider store={store}>
-        <Router history={browserHistory} routes={routes} />
+        <RecipeShow routeParams={{ id: 5 }}/>
       </Provider>
     );
   });
 
   it('should render', () => {
     expect(item).to.exist;
+  });
+
+  it('should render recipe list with id: 5', () => {
+    const recipe5 = store.getState().recipes.all.find(recipe => recipe.id === 5);
+    expect(item.find('h3').first().text()).to.equal(recipe5.name);
+  });
+
+  it('should have Edit button', () => {
+    expect(item.findWhere(el => (
+      (el.type() === 'a' || el.type() === 'button') && el.text() === 'Edit'
+    )).first()).to.exist;
+  });
+
+  it('should have Delete button', () => {
+    expect(item.findWhere(el => (
+      (el.type() === 'a' || el.type() === 'button') && el.text() === 'Delete'
+    )).first()).to.exist;
+  });
+
+  it('should have Back To Recipe List button', () => {
+    expect(item.findWhere(el => (
+      (el.type() === 'a' || el.type() === 'button') && el.text() === 'Back To Recipe List'
+    )).first()).to.exist;
   });
 });
